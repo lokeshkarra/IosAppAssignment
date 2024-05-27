aca4cec18084f35a30af7058f468ffd03ae3f486
lokeshkarra <lokeshwarreddy.karra@gmail.com>
2024-05-28 03:35:06 +0800

    Initial Commit

---
+++ IosApp/IosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosAppApp.swift
+//  IosApp
+//
+//  Created by user263596 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct IosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
